c2b8e370b7bd689aa3c4d46e1ae3bc56ce00aa0e
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-03-22 10:40:26 -0400

    Created the main AES encryption function and plan for implementation

--- AES_Encryption.java
+++ AES_Encryption.java
@@ -12,4 +12,24 @@ public class AES_Encryption{
         String key = "000102030405060708090a0b0c0d0e0f1011121314151617";
         String keysize = "192 bits";
     }
+
+    //Main  function for encrypting plaintext using AES implementation from scratch
+    //Should take in the plaintext, key, and key sizes as input and output a ciphertext
+    public static String encryptingPlaintext(String plaintext, String key, int keysize)
+    {
+        /*
+         * Pseudocode:
+         * Additionally, create a 4*4 matrix to store the plaintext in HEX form
+         * Repeating 4 steps of AES depending on the keysize
+         * Complete subytes steo,
+         * shiftrows step
+         * mixcolumns step
+         * and addround key step
+         * Return the new ciphertext
+         * Note: For round 0, only implement the add round key step and use this new key going forward
+         */
+        return "";
+    }
+
+
 }
